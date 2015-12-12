36aa58d7e6f846959b0ea2a345e11a26860f17f9
Kracav4ik <dikama2013@yandex.ru>
2015-12-13 02:59:59 +0300

    pygame: игровой цикл

--- main.py
+++ main.py
@@ -11,12 +11,26 @@ window_bg = (128, 255, 255)
 
 screen = pygame.display.set_mode(window_size)
 
-while True:
+
+def handle_input():
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             sys.exit()
 
-    screen.fill(window_bg)
 
+def process_game():
+    pass
+
+
+def render():
+    main_screen = pygame.display.get_surface()
+    main_screen.fill(window_bg)
     pygame.display.flip()
+
+
+# main game cycle
+while True:
+    handle_input()
+    process_game()
+    render()
