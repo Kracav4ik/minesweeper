0b9b06f37c1c1605be6b60e2e5e46c35465da46f
Kracav4ik <dikama2013@yandex.ru>
2015-12-13 02:42:02 +0300

    pygame: пустое окно цвета неба

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import sys
 pygame.init()
 
 window_size = (1280, 720)
+window_bg = (128, 255, 255)
 
 screen = pygame.display.set_mode(window_size)
 
@@ -15,3 +16,7 @@ while True:
     for event in events:
         if event.type == pygame.QUIT:
             sys.exit()
+
+    screen.fill(window_bg)
+
+    pygame.display.flip()
